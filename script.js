906751d418843ce2031ebe16d46f7f39d0c499f7
“Ram” <ramjondhale1@gmail.com>
2020-09-14 14:12:21 +0530

    theme setting added

---
+++ script.js
@@ -0,0 +1,30 @@
+let theme = localStorage.getItem('theme')
+
+if (theme == null) {
+    setTheme('light')
+} else {
+    setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i = 0; themeDots.length > i; i++) {
+    themeDots[i].addEventListener('click', function() {
+        let mode = this.dataset.mode
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode) {
+    if (mode == 'light') {
+        document.getElementById('theme-style').href = 'default.css'
+    } else if (mode == 'blue') {
+        document.getElementById('theme-style').href = 'blue.css'
+    } else if (mode == 'green') {
+        document.getElementById('theme-style').href = 'green.css'
+    } else {
+        document.getElementById('theme-style').href = 'purple.css'
+    }
+
+    localStorage.setItem('theme', mode)
+}
